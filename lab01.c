b20d612796d3a3a23731921b482c241059a5a38e
Steve <steve.blandon@udea.edu.co>
2021-03-21 20:43:50 -0500

    ingredients issue

--- lab01.c
+++ lab01.c
@@ -7,20 +7,20 @@ int main (int argc, char*argv[]){
 		printf("You must specify a filepath\n");
 		return EXIT_FAILURE;
 	}
-	FILE*fp=fopen(argv[1],"r");
+	FILE *fp=fopen(argv[1],"r");
 
 	if(!fp){
 		printf("Error while opening the file %s\n",argv[1]);
 	}
-	char line[1024];
+	char line[2048];
 	int linecount= 0;
 	int quantities[4];
 	int termscount=0;
 	int numingredients=0;
-	char *ingredientsdif[10];
+	char *ingredientsdif[1024];
 	int ingredients=0;
 	bool band;
-	while(fgets(line,1024,fp)){
+	while(fgets(line,2048,fp)){
 		char *word;
 		char *rest=line;
 		if(linecount==0){
@@ -37,25 +37,31 @@ int main (int argc, char*argv[]){
 				word= strtok_r(rest," ", &rest);
 				band=false;
 
-				for(int a=0;a<=10;a++){
-					if(strcmp(ingredientsdif[a],word)==0){
+				for(int a=0;a<ingredients;a++){
+					int result =strcmp(ingredientsdif[a],word);
+					printf("word %s\n",word);
+					printf("ingredientdif %s\n",ingredientsdif[a]);
+					printf("%d\n",result);
+					if(result==0){
 						band=true;
 					}
 				}
 				if(band==false){
+					printf("new ingredient %s\n",word);
 					ingredientsdif[ingredients]=word;
-/*					ingredients++;*/
+					printf("vector ingredient %s\n",ingredientsdif[ingredients]);
+					ingredients++;
 				}
-				printf("ingredient: %s\n", word);
+				printf("all ingredient: %s", word);
 			}
 		}
 	}
-	for(int i=0;i<4;i++){
-		printf("quantity: %d\n",quantities[i]);
+	for(int b=0;b<4;b++){
+		printf("quantity: %d\n",quantities[b]);
 	}
-/*	for(int i=0;i<10;i++){
-		printf("ingredients: %s\n",ingredientsdif[i]);
+	for(int c=0;c<ingredients;c++){
+		printf("iterate vector ingredients: %s ",ingredientsdif[c]);
 	}
-	printf("number of diferent ingredients %d\n",ingredients);*/
+	printf("number of diferent ingredients %d\n",ingredients);
 	fclose(fp);
 }
