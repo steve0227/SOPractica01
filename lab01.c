7f3ffcd186a2919c4b1e55040d2f368e044f23ec
Steve <steve.blandon@udea.edu.co>
2021-03-25 20:10:06 -0500

    platos x ingredientes

--- lab01.c
+++ lab01.c
@@ -16,39 +16,14 @@ void remove_spaces(char* restrict str_trimed,const char* restrict str_untrimmed)
 	}
 	*str_trimed='\0';
 }
-int main (int argc, char*argv[]){
-	if (argc < 2){
-		printf("You must specify a filepath\n");
-		return EXIT_FAILURE;
-	}
-	FILE *fp=fopen(argv[1],"r");
 
-	if(!fp){
-		printf("Error while opening the file %s\n",argv[1]);
-	}
+int get_ingredients_quantities(int quantities[], char ingredientsdif[20][10], FILE * restrict fp){
 	char line[1024];
 	int linecount= 0;
-	int quantities[4];
 	int termscount=0;
 	int numingredients=0;
-	char ingredientsdif[10][10];
 	int ingredients=0;
 	bool band;
-	
-/*	char *a1="ajo ";
-	char *a2="ajo";
-	char a3[2][6];
-	strcpy(a3[1],a2);
-	int r1=strcmp(a1,a2);
-	int r2=strcmp(a1,a3[1]);
-	printf("r1: %d\n",r1);
-	printf("r2: %d\n",r2);
-	char *a4=(char*)malloc(sizeof(char*));
-	remove_spaces(a4,a1);
-	int r3=strcmp(a4,a3[1]);
-	printf("r3: %d\n",r3);*/
-
-	
 	while(fgets(line,1024,fp)){
 		char *word;
 		char *rest=line;
@@ -69,23 +44,88 @@ int main (int argc, char*argv[]){
 				remove_spaces(aux,word);
 				for(int a=0;a<ingredients;a++){
 					int result =strcmp(ingredientsdif[a],aux);
-					printf("word %s\n",word);
-					printf("ingredientdif %s\n",ingredientsdif[a]);
-					printf("%d\n",result);
 					if(result==0){
 						band=true;
 					}
 				}
 				if(band==false){
-					printf("new ingredient %s\n",word);
 					strcpy(ingredientsdif[ingredients],aux);
-					printf("vector ingredient %s\n",ingredientsdif[ingredients]);
 					ingredients++;
 				}
-				printf("all ingredient:%s",word);
 			}
 		}
 	}
+	return ingredients;
+}
+void fill_p_matriz(int ingredients, int np, char ingredientsdif[ingredients][10], int p[np][ingredients],int line, char* restrict aux){
+	for(int i=0;i<ingredients;i++){
+		int result=strcmp(ingredientsdif[i],aux);
+/*		printf("vector %s\n",ingredientsdif[i]);
+		printf("palabra %s\n",aux);
+		printf("result %d\n",result);*/
+		if(result==0){
+			p[line-1][i]=1;
+			i=ingredients;
+/*			printf("i value %d\n",i);
+			printf("p value %d\n",p[line][i]);*/
+		}
+	}
+}
+
+
+void get_p_matriz(int ingredients, int np, char ingredientsdif[ingredients][10], int p[np][ingredients],FILE* restrict fp){
+	char line[1024];
+	int linecount= 0;
+	int numingredients=0;
+	while(fgets(line,1024,fp)){
+		char *word;
+		char *rest=line;
+		if(linecount==0){
+/*			word=strtok_r(rest," ",&rest);
+			int np = atoi(word);
+			int p[np][ingredients]*/
+			linecount++;
+		}else{
+			word= strtok_r(rest," ", &rest);
+			numingredients = atoi(word);
+			for(int i =0; i<numingredients;i++){
+				word= strtok_r(rest," ", &rest);
+				char *aux=(char*)malloc(sizeof(char*));
+				remove_spaces(aux,word);
+				fill_p_matriz(ingredients,np,ingredientsdif,p,linecount,aux);
+			}
+			linecount++;
+		}
+	}
+}
+
+int main (int argc, char*argv[]){
+	if (argc < 2){
+		printf("You must specify a filepath\n");
+		return EXIT_FAILURE;
+	}
+	FILE *fp=fopen(argv[1],"r");
+
+	if(!fp){
+		printf("Error while opening the file %s\n",argv[1]);
+	}
+
+	int quantities[4];
+	char ingredientsdif[20][10];
+	int ingredients=get_ingredients_quantities(quantities,ingredientsdif,fp);
+	fclose(fp);
+	int np=quantities[0];
+	int ntpp=quantities[1]*2+quantities[2]*3+quantities[3]*4;
+	int p[np][ingredients];
+	int ap[ntpp];
+
+	int op2=2*quantities[1];
+	int op3=op2+3*quantities[2];
+
+	fp=fopen(argv[1],"r");
+	get_p_matriz(ingredients,np,ingredientsdif,p,fp);
+	fclose(fp);
+
 	for(int b=0;b<4;b++){
 		printf("quantity: %d\n",quantities[b]);
 	}
@@ -93,7 +133,19 @@ int main (int argc, char*argv[]){
 		printf("iterate vector ingredients: %s\n ",ingredientsdif[c]);
 	}
 	printf("number of diferent ingredients %d\n",ingredients);
-	fclose(fp);
+
+	for(int i =0;i<quantities[0];i++){
+		for(int j=0;j<ingredients;j++){
+			if(p[i][j]!=1){
+				printf("0");
+			}
+			else{
+				printf("%d",p[i][j]);
+			}
+		}
+		printf("\n");
+	}
+
 }
 
 
